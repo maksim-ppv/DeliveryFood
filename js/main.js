cdc86b1b93e6d436c15a2fbb5708c94a34bc6a28
maksim-ppv <popovm1502@gmail.com>
2020-04-13 01:22:58 +0300

    Анимация лого со скриптом

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
 const cartButton = document.getElementById('cart-button');
 const modal = document.querySelector('.modal');
-const modalClose = document.querySelector('.close')
+const modalClose = document.querySelector('.close');
+const logo = document.querySelectorAll('.logo');
 
 
-cartButton.addEventListener('click', e =>{
+cartButton.addEventListener('click', e => {
     modal.classList.add('is-open')   
 });
 
-modalClose.addEventListener('click', e=>{
+modalClose.addEventListener('click', e=> {
     modal.classList.remove('is-open')
 });
 
-new WOW().init();
+
+const animationActive = (i, time, animeActive, animeDeactive) => {
+    i.addEventListener('mouseover', e=>{
+        i.style.animationName = animeActive;
+        i.style.animationDuration = time+'s';
+        if(i.classList.contains('wow')){
+            i.classList.remove('wow', animeDeactive)
+        };
+        setTimeout(() => i.style.animationName='', time*1000-100)
+    });
+};
+
+logo.forEach(i=>{
+    animationActive(i, 1, 'pulse', 'fadeInRight');
+});
+wow = new WOW({
+    mobile: false,
+    offset: 100
+
+})
+wow.init();
